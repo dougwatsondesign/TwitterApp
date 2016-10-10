ca49395ff1520ce1238ddd87a4b9c71080e821d0
DOUG WATSON <DOUGWATSONBUSINESS@GMAIL.COM>
2016-10-10 17:31:11 -0400

    first commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,46 @@
+class TweetsController < ApplicationController
+	before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+	before_filter :authenticate_user!
+
+
+	def index
+		@t = Tweet.all 
+	end
+
+	def new
+		@tweet = Tweet.new
+	end
+
+	def show
+	end
+
+	def create
+		@tweet = Tweet.new(tweet_params)
+		if @tweet.save
+			redirect_to @tweet, notice: "Tweet was successfully created!"
+		else
+			render :new
+		end
+end
+
+	def update
+		if @tweet.update(tweet_params)
+			redirect_to @tweet, notice: "Tweet was successfully updated"
+		else
+			render :edit
+		end
+	end
+
+
+private
+
+	def set_tweet
+		@tweet = Tweet.find(params[:id])
+	end
+
+	def tweet_params
+		params.require(:tweet).permit(:message, :user_id)
+	end
+
+
+end
